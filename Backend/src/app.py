940deaa224a647d9e6a08fdf21865f84050e3641
Andrea Alvarez <andreaalpo23@gmail.com>
2025-11-11 19:57:36 -0500

    correcciones

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -5,12 +5,29 @@ from routes import Product
 from config import config
 
 app = Flask(__name__)
-swagger = Swagger(app)  # inicializamos Swagger
 CORS(app)
+# swagger = Swagger(app)  # inicializamos Swagger
+
+
 def page_not_found(e):
     return "La página que buscas no existe", 404
 
+@app.route('/')
+def home():
+    return {
+        'message': 'Backend Flask funcionando correctamente 🚀',
+        'swagger_docs': '/apidocs/'
+    }
 
+# Blueprints
+app.register_blueprint(Product.main, url_prefix='/api/productos')
+
+# Configuración Swagger
+app.config['SWAGGER'] = {
+    'title': 'API Inventario - Tienda de barrio',
+    'uiversion': 3,
+}
+swagger = Swagger(app)
 
 
 if __name__ == '__main__':
@@ -18,8 +35,8 @@ if __name__ == '__main__':
     app.config.from_object(config['development'])
     
 
-    #blueprints
-    app.register_blueprint(Product.main, url_prefix='/api/productos')
+    # #blueprints
+    # app.register_blueprint(Product.main, url_prefix='/api/productos')
 
     #manejador de error
     app.register_error_handler(404, page_not_found)
